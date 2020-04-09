94ee2747a01d6e4b8397d919c142030c8ffe0c5f
Mario Alberquilla <malberquilla@gmail.com>
2020-04-09 21:57:44 +0200

    Alert page

--- flutter-components/lib/src/pages/alert_page.dart
+++ flutter-components/lib/src/pages/alert_page.dart
@@ -6,8 +6,13 @@ class AlertPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Alert page')
+      appBar: AppBar(title: Text('Alert page')),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Show alert'),
+            color: Colors.blue,
+            shape: StadiumBorder(),
+            onPressed: () => _showAlert(context)),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
@@ -16,5 +21,32 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+            title: Text('Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Alert content'),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Ok'))
+            ],
+          );
+        });
   }
 }
