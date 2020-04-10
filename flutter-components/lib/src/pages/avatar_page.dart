a92aa361524ce509bbbf0c4eb5178049ab129d06
Mario Alberquilla <malberquilla@gmail.com>
2020-04-10 14:30:15 +0200

    Avatar page and AnimatedContainer page

--- flutter-components/lib/src/pages/avatar_page.dart
+++ flutter-components/lib/src/pages/avatar_page.dart
@@ -7,8 +7,33 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar page')
-      )
+        title: Text('Avatar page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.cinepremiere.com.mx/wp-content/uploads/2018/07/stanleeanimado.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+            placeholder: AssetImage('assets/original.gif'),
+            image: NetworkImage(
+                'https://www.elsoldeacapulco.com.mx/incoming/wpgvi0-villanos-creados-por-stan-lee.jpg/ALTERNATES/LANDSCAPE_1140/Villanos%20creados%20por%20Stan%20lee.jpg'),
+                fadeInDuration: Duration(milliseconds: 250),
+                ),
+      ),
     );
   }
 }
